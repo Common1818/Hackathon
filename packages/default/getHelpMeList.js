cc7501a81184e95a8a27849550e60fb5c4f38140
PriyaBihani <priyabihani.official@gmail.com>
2020-10-24 14:31:08 +0530

    djkh

---
+++ packages/default/getHelpMeList.js
@@ -0,0 +1,67 @@
+function main(){
+    console.log("working")
+    return {
+        patients: [
+            {
+              name: "Steve Trabajo",
+              city: "New Delhi",
+              hospital: "Sir Ganga ram hospital , new rajendra nagar , New Delhi",
+              needs: "Plasma",
+              bloodGroup: "AB-",
+              description:
+                "nsectetur adipiscing elit. Maecenas a faucibus diam. Praesent consectetur, nisl ut semper imperdiet, tortor metus ultrices lorem, non luctus massa risus at urna.",
+              phone: "9876543210",
+              whatsapp: "9876543210",
+              instagram: "213lkjas",
+            },
+            {
+              name: "Monster King",
+              city: "Guru Gram",
+              hospital: "Fortis Hospital, New Delhi",
+              needs: "Blood",
+              bloodGroup: "AB-",
+              description:
+                "nsectetur adipiscing elit. Maecenas a faucibus diam. Praesent consectetur, nisl ut semper imperdiet, tortor metus ultrices lorem, non luctus massa risus at urna.",
+              phone: "9876543210",
+              whatsapp: "9876543210",
+              instagram: "213lkjas",
+            },
+            {
+              name: "Steve Trabajo",
+              city: "New Delhi",
+              hospital: "Sir Ganga ram hospital , new rajendra nagar , New Delhi",
+              needs: "Plasma",
+              bloodGroup: "AB-",
+              description:
+                "nsectetur adipiscing elit. Maecenas a faucibus diam. Praesent consectetur, nisl ut semper imperdiet, tortor metus ultrices lorem, non luctus massa risus at urna.",
+              phone: "9876543210",
+              whatsapp: "9876543210",
+              instagram: "213lkjas",
+            },
+            {
+              name: "Monster King",
+              city: "Guru Gram",
+              hospital: "Fortis Hospital, New Delhi",
+              needs: "Blood",
+              bloodGroup: "AB-",
+              description:
+                "nsectetur adipiscing elit. Maecenas a faucibus diam. Praesent consectetur, nisl ut semper imperdiet, tortor metus ultrices lorem, non luctus massa risus at urna.",
+              phone: "9876543210",
+              whatsapp: "9876543210",
+              instagram: "213lkjas",
+            },
+          ],
+
+          students:[
+              {
+                  name: 'priya',
+                  city: "New Delhi",
+
+              },
+              {
+                  name: 'Kartik bhsdiwale',
+                  city: "New Delhi",
+              }
+          ]
+    }
+}
